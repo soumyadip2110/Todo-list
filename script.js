e99d3f2552864b7497d58ce21b68ca7adc1115da
Soumyadip Chowdhury <soumyadip2110@gmail.com>
2024-07-10 20:34:42 +0530

    make code clean, add input validation and event checks

--- script.js
+++ script.js
@@ -1,51 +1,39 @@
 window.onload = displayTasks()
 const addTaskBtn = document.getElementById('add-task')
 
+window.addEventListener('keydown', (e) => {
+    if (e.key == 'Enter'){
+        addTaskBtn.click()
+    }
+})
+
 addTaskBtn.addEventListener('click', () => {
-    let taskInput = document.getElementById('task-input').value
-    saveTask(taskInput)
-    displayTasks()
+    let taskInput = document.getElementById('task-input')
+    if (taskInput.value){
+        saveTask(taskInput.value)
+        displayTasks()
+    }
+
+    taskInput.value = ''
 })
 
 function addNewTask(task, index) {
     const allTasksBox = document.querySelector('.all-tasks-container-secondary')
 
-    const taskDiv = document.createElement('div')
-    taskDiv.setAttribute('class', 'task-box')
-
-    const taskTextDiv = createTaskTextDiv(task)
+    const taskDiv = createElement('div', 'task-box', '')
+    const taskTextDiv = createElement('div', 'task-text-box', task)
     const buttonsDiv = createButtons(index)
-    // const editBtn = createEditBtn()
-    // const removeBtn = createRemoveBtn()
-
-    // addEditEvent(editBtn, index)
-    // addRemoveEvent(removeBtn, index)
 
     taskDiv.appendChild(taskTextDiv)
     taskDiv.appendChild(buttonsDiv)
-    // taskDiv.appendChild(editBtn)
-    // taskDiv.appendChild(removeBtn)
 
     allTasksBox.appendChild(taskDiv)
-
-}
-
-function createTaskTextDiv(text) {
-    const taskTextDiv = document.createElement('div')
-    taskTextDiv.setAttribute('class', 'task-text-box')
-
-    const taskText = document.createTextNode(text)
-    taskTextDiv.appendChild(taskText)
-
-    return taskTextDiv
 }
 
 function createButtons(index) {
-    const div = document.createElement('div')
-    div.setAttribute('class', 'buttons-container')
-
-    const editBtn = createEditBtn()
-    const removeBtn = createRemoveBtn()
+    const div = createElement('div', 'buttons-container', '')
+    const editBtn = createElement('button', 'edit-btn', 'Edit')
+    const removeBtn = createElement('button', 'remove-btn', 'Remove')
 
     addEditEvent(editBtn, div, index)
     addRemoveEvent(removeBtn, index)
@@ -55,73 +43,38 @@ function createButtons(index) {
     return div
 }
 
-function createEditBtn() {
-    const editBtn = document.createElement('button')
-    editBtn.setAttribute('class', 'edit-btn')
-
-    const editBtnText = document.createTextNode('Edit')
-    editBtn.appendChild(editBtnText)
-
-    return editBtn
-}
-
-function createDoneBtn() {
-    const doneBtn = document.createElement('button')
-    doneBtn.setAttribute('class', 'done-btn')
-    doneBtn.appendChild(document.createTextNode('Done'))
-    return doneBtn
-}
-
-function createRemoveBtn() {
-    const removeBtn = document.createElement('button')
-    removeBtn.setAttribute('class', 'remove-btn')
-
-    const removeBtnText = document.createTextNode('Remove')
-    removeBtn.appendChild(removeBtnText)
-
-    return removeBtn
-}
-
 function addEditEvent(btn, btnContainer, index) {
     btn.addEventListener('click', () => {
-        const allTasks = JSON.parse(window.localStorage.getItem('tasks'))
-
-        let editableTask = allTasks.filter((task, idx) => idx === index)
-        // in arrow func. '{idx === index}' is not valid as, if '{}' is included then there must be a 'return' statement written explicitely
-
-        // let editableTask = allTasks.filter((task, idx) => {
-        //     if (idx == index){
-        //         console.log(task)
-        //         return task
-        //     }
-        // })
-        // console.log(editableTask);
-
-        // NOTE: THIS METHOD SELECTS ONLY THE TASK TEXT DATA FROM THE LOCAL STORAGE
-
         const btnParentDiv = btn.parentElement
         const textDiv = btnParentDiv.previousElementSibling
-        // use 'previousElementSibling' instead of 'previousSibling' to get the previous element node (skipping text nodes and any other non-element nodes).
-        textDiv.setAttribute('contentEditable', 'true')
+        // NOTE: use 'previousElementSibling' instead of 'previousSibling' to get the previous element node 
+        // (skipping text nodes and any other non-element nodes).
 
-        const doneBtn = createDoneBtn()
-        btnContainer.prepend(doneBtn)
+        if (!textDiv.hasAttribute('contentEditable')){
+            textDiv.setAttribute('contentEditable', 'true')
 
-        addDoneEvent(doneBtn, textDiv, index)
+            const doneBtn = createElement('button', 'done-btn', 'Done')
+            btnContainer.prepend(doneBtn)
+
+            addDoneEvent(doneBtn, textDiv, index)
+        }
     })
 }
 
 function addDoneEvent(btn, textDiv, index){
     btn.addEventListener('click', () => {
         const editedTaskText = textDiv.textContent
-        // console.log(editedTaskText);
-        const allTasks = JSON.parse(window.localStorage.getItem('tasks'))
-        const updatedTasks = allTasks.map((task, idx) => {
-            return idx === index ? editedTaskText : task
-        })
-        // console.log(updatedTasks);
-        window.localStorage.setItem('tasks', JSON.stringify(updatedTasks))
-        btn.remove()
+        if (editedTaskText.trim().length > 0){
+            const allTasks = JSON.parse(window.localStorage.getItem('tasks'))
+
+            const updatedTasks = allTasks.map((task, idx) => {
+                return idx === index ? editedTaskText : task
+            })
+
+            window.localStorage.setItem('tasks', JSON.stringify(updatedTasks))
+            btn.remove()
+            textDiv.setAttribute('contentEditable', 'false')
+        }
     })
 }
 
@@ -134,25 +87,39 @@ function addRemoveEvent(btn, index) {
                 return idx !== index;
             })
         )
+        
         window.localStorage.setItem('tasks', updatedAllTasks)
         displayTasks()
     })
 }
 
 function saveTask(task) {
-    let allTasks = JSON.parse(window.localStorage.getItem('tasks')) || []
-    allTasks.push(task)
-    allTasks = JSON.stringify(allTasks)
-    window.localStorage.setItem('tasks', allTasks)
+    if (task.trim().length > 0){
+        let allTasks = JSON.parse(window.localStorage.getItem('tasks')) || []
+        allTasks.push(task)
+        allTasks = JSON.stringify(allTasks)
+        window.localStorage.setItem('tasks', allTasks)
+    }
 }
 
 function displayTasks() {
     const allTasksBox = document.querySelector('.all-tasks-container-secondary')
     allTasksBox.innerHTML = ''
+
     const allTasks = JSON.parse(window.localStorage.getItem('tasks'))
+
     if (allTasks) {
         allTasks.forEach((task, index) => {
             addNewTask(task, index)
         })
     }
+}
+
+function createElement(elementName, className, text){
+    const element = document.createElement(elementName)
+    element.setAttribute('class', className)
+    if (text){
+        element.appendChild(document.createTextNode(text))
+    }
+    return element
 }
